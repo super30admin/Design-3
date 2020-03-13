3fd8a9c10adbd3fbafcd1da1e42a615b637300a6
Brandon Goodman <Bran99@users.noreply.github.com>
2020-03-13 10:24:06 -0400

    Update with comments

--- flatten-nested-list-iterator.js
+++ flatten-nested-list-iterator.js
@@ -34,6 +34,7 @@
  * @param {NestedInteger[]} nestedList
  */
 var NestedIterator = function(nestedList) {
+    // Initialize our stack using the nestedList (in reverse order)
     this.stack = [];
     for (let i = nestedList.length - 1; i >= 0; i--) {
         this.stack.push(nestedList[i]);
@@ -47,15 +48,22 @@ var NestedIterator = function(nestedList) {
  */
 NestedIterator.prototype.hasNext = function() {
     while (this.stack.length > 0) {
+        // If the last el in my stack is an integer, return true
         if (this.stack[this.stack.length - 1].isInteger()) return true;
         else {
+            // Otherwise, keep calling getList on the last element
+            // And putting it back at the top of the stack
+            // Until we get an integer
             let curr = this.stack[this.stack.length - 1].getList();
             this.stack.pop();
             for (let i = curr.length - 1; i >= 0; i--) {
+                // Add the elements of the list one by one to the top
+                // Of the stack
                 this.stack.push(curr[i]);
             }
         }
     }
+    // In the case that the stack is empty, return false
     return false;
 };
 
@@ -64,6 +72,8 @@ NestedIterator.prototype.hasNext = function() {
  * @returns {integer}
  */
 NestedIterator.prototype.next = function() {
+    // Call hasNext and then get the element off the top of the stack
+    this.hasNext();
     return this.stack.pop().getInteger();
 };
 
