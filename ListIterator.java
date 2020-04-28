89386f380f9f1ae804fe32d5073429514916e35b
RutujaKaushike <rutujakaushike@gmail.com>
2020-04-27 19:22:24 -0500

    Updated code for Iterator

--- ListIterator.java
+++ ListIterator.java
@@ -4,9 +4,7 @@
 // Any problem you faced while coding this : No
 
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class ListIterator {
     /**
@@ -26,15 +24,29 @@ public class ListIterator {
      *     public List<NestedInteger> getList();
      * }
      */
+    class NestedInteger1{
+        public boolean isInteger(){
+            return true;
+        }
+
+        public int getInteger(){
+            return 0;
+        }
+
+        public List<NestedInteger1> getList(){
+            return new ArrayList<NestedInteger1>();
+        }
+    }
+
      class NestedIterator implements Iterator<Integer> {
         Queue<Integer> q;
-        public NestedIterator(List<NestedInteger> nestedList) {
+        public NestedIterator(List<NestedInteger1> nestedList) {
             q = new LinkedList<>();
             addAll(nestedList);
         }
 
-        private void addAll(List<NestedInteger> nestedList){
-            for(NestedInteger n : nestedList){
+        private void addAll(List<NestedInteger1> nestedList){
+            for(NestedInteger1 n : nestedList){
                 if(n.isInteger()){
                     q.add(n.getInteger());
                 } else {
@@ -53,9 +65,43 @@ public class ListIterator {
         }
     }
 
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * NestedIterator i = new NestedIterator(nestedList);
- * while (i.hasNext()) v[f()] = i.next();
- */
+    /**
+     * Your NestedIterator object will be instantiated and called as such:
+     * NestedIterator i = new NestedIterator(nestedList);
+     * while (i.hasNext()) v[f()] = i.next();
+     */
+
+    // Google Type Code
+    Stack<ListIterator<NestedInteger>> st;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        st = new Stack<>();
+        st.push(nestedList.listIterator());
+    }
+
+    @Override
+    public Integer next() {
+        hasNext();
+        return st.peek().next().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!st.isEmpty()){
+
+            if(!st.peek().hasNext()){
+                st.pop();
+            } else {
+                NestedInteger x = st.peek().next();
+                if(x.isInteger()){
+                    return st.peek().previous() == x;
+                } else {
+                    st.push(x.getList().listIterator());
+                }
+            }
+        }
+
+        return false;
+    }
+
+
 }
