e0301158f590fa35dd9b4578526c62c0afe0c42b
laxmikantbpandhare <laxmikantbhaskar.pandhare@sjsu.edu>
2020-09-27 23:46:20 -0700

    add changes

--- lruCache.java
+++ lruCache.java
@@ -10,11 +10,19 @@
         # Optimized approach:
                               
              approach   
-                    A. We will use queue to store the data.
-                    B. Do the recusion on the nestedList. if it is a integer then add to the queue
-                    C. If not the do the recusion call with that list.
-                    D. For next(), just poll the queue.
-                    E. for hasNext, return is queue is not empty.
+                    A. In order to solve this problem, in o(1) time complexity of put and get operation,
+                       we need to use hashmap wirh node added as a doubly linkedlist.
+                    B. We will first create a doubly linkedlist as mentioed in the below.
+                    C. then add operator where we willa dd our element to the last in the linkedlist.
+                    D. In remove, we will just  point  out the before and after node to each other.
+                    E. In update, we will remove the element first  and then add it to the end.
+                    F. in put method, first check if element is present in hashmap or not.
+                       If not, then simply put it into hashmap and add to the linkedlist.
+                       If yes, then update() methoda gets invoked.
+                    G. in  get  method, first check if element is present in hashmap or not.
+                       If not, then simply return -1;
+                       If yes, move element to the end as it is recently used. so update() operator and 
+                       return the value of that updated node.
 
        */
 
