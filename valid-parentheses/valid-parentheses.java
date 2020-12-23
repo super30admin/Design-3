4d1ef2ff84eb5cdd810447f8b6c01d51d85451a8
pkariya1 <pkariya1@binghamton.edu>
2020-12-23 11:33:44 +0530

    Time: 1 ms (98.86%), Space: 37 MB (87.93%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> str = new Stack<>();
+        
+        
+       for(int i = 0 ;i< s.length();i++){
+           Character ch = s.charAt(i);
+           
+           if(ch == '('){
+               str.push(')');
+           }else if(ch == '{'){
+               str.push('}');
+           }else if(ch == '['){
+               str.push(']');
+           }else if(str.isEmpty() || str.pop()!=ch) {
+              return false;
+           }
+           
+           
+       }
+        
+        if(!str.isEmpty()) return false;
+​
+        return true;
+        
+        
+    }
+}
