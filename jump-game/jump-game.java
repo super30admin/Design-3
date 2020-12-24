fed418fe0a239295ea99c36af159c619da5c0144
pkariya1 <pkariya1@binghamton.edu>
2020-12-24 15:04:39 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (32.85%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -5,7 +5,7 @@ class Solution {
         
         int destination = nums.length -1;
         int n = nums.length -1;
-        for(int i = n; i>=0;i-- ){
+        for(int i = n-1; i>=0;i-- ){
             if(i+nums[i]>= destination){
                 destination = i;
             }
