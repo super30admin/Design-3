ac96f098f32a17826b7e2e8da8fbba23dc36becd
pkariya1 <pkariya1@binghamton.edu>
2020-12-24 14:18:34 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (75.07%) - LeetHub

---
+++ jump-game/jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums==null || nums.length == 0)
+            return false;
+        
+        int destination = nums.length -1;
+        int n = nums.length -1;
+        for(int i = n; i>=0;i-- ){
+            if(i+nums[i]>= destination){
+                destination = i;
+            }
+        }
+        return destination ==0;
+        
+    }
+}
