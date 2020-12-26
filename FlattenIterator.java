706c917c89a9a6cac680ebf8aa1cc8889fc5439d
harsh <harshsaini697@gmail.com>
2020-12-26 17:32:54 -0600

    stack flatten

--- FlattenIterator.java
+++ FlattenIterator.java
@@ -3,6 +3,51 @@
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :No
 
+
+public class NestedIterator implements Iterator<Integer> {
+
+    Stack<Iterator<NestedInteger>> stack;
+    
+    NestedInteger nextElement;
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        this.stack = new Stack<>();
+        stack.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return nextElement.getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        while(!stack.isEmpty()){
+            
+            if(!stack.peek().hasNext()){
+                stack.pop();
+            }
+            else if((nextElement = stack.peek().next()).isInteger()){ 
+                // stack might become empty after previous pop, hence the else if
+                return true;
+            }
+            else{
+                stack.push(nextElement.getList().iterator());
+            }
+        }
+        return false;
+
+    }
+}
+
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+/*
 public class NestedIterator implements Iterator<Integer> {
     List<Integer> result;
     Iterator<Integer> it;
@@ -33,3 +78,4 @@ public class NestedIterator implements Iterator<Integer> {
         return it.hasNext();
     }
 }
+*/
