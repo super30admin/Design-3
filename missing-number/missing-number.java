6327161b41b391c6d0a68e71c5ae9d3fcfc13d9e
pkariya1 <pkariya1@binghamton.edu>
2021-01-02 14:26:51 +0530

    Time: 5 ms (25.77%), Space: 39.4 MB (68.48%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -3,19 +3,19 @@ class Solution {
        if (nums.length == 0 || nums == null) {
             return -1;
         }
-        Arrays.sort(nums);
+        Arrays.sort(nums); //O(nlogn)
         int low =0;
         int high = nums.length -1;
-        while(low<=high){   //[0,1,2,3,4,5,6,7,9]
-            int mid = low + (high-low)/2;
-            if(nums[mid] - mid!=0){
-                high = mid -1;
+        while(low<=high){   //[0,1,2,3,4,5,6,7,9]    // O(logn)
+            int mid = low + (high-low)/2; // 8 + (9-8)/2 = 8
+            if(nums[mid] - mid!=0){ //9-8 == 1
+                high = mid -1; 
             }else{
                 low = mid+1;
             }
         }
         
-        return low;
-        
+        return low; // 8
+        //total time complexity ==> (O(n* 2logn))
     }
 }
