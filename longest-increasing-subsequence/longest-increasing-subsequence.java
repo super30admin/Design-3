1bf536c8064f3f2e8d8e24fe8717f69f34c36b53
pkariya1 <pkariya1@binghamton.edu>
2021-01-06 15:31:29 +0530

    Time: 2 ms (95.20%), Space: 38.3 MB (78.97%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLISUsingDp(int[] nums) {
        if (nums == null || nums.length == 0)
             return 0;
 ​
@@ -18,5 +18,40 @@ class Solution {
         return max;
         
         
-    }
-}
+    }
+    
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int len = 0;
+        int[] dp = new int[nums.length];
+         dp[0] = nums[0];
+        len++;
+        for(int i = 1 ;i< nums.length; i++){
+                if(dp[len-1] < nums[i]){
+                    dp[len] = nums[i];
+                    len++;
+                }else{
+                   int index = binarySearch(dp,0,len-1,nums[i]);
+                    dp[index] = nums[i];
+                }
+            
+        }
+        
+        return len;
+        
+           
+    }
+    private int binarySearch(int [] nums,int low , int high, int target){
+        
+        while(low<=high){
+            
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                
+                return mid;
+            }else if(nums[mid] >target) high = mid -1;
+            else
+                low = mid +1 ;
+            
+        }
