88b84375d76f6dfece0ecb04dc88f4e1ad78bb9d
gangakalburgi <56897430+gangakalburgi@users.noreply.github.com>
2021-01-29 16:44:45 -0800

    Update LruCache.java

--- LruCache.java
+++ LruCache.java
@@ -34,6 +34,8 @@ class Node{
     }
     
 }
+//creating doubly linkedlist
+
 class DoublyLinkedList{
     Node head;
     Node tail;
@@ -46,17 +48,14 @@ class DoublyLinkedList{
         tail.prev = head;
     }
     
+    //method to insert at head
     public void insertHead(Node n){
         n.prev = head;
         n.next = head.next;
         head.next.prev = n;
         head.next = n;
     }
-    
-    public void remove(Node n){
-        n.prev.next = n.next;
-        n.next.prev = n.prev;
-    }
+     //this the method to remove from tail
     public int removeTail(){
         Node n = tail.prev;
         int k = n.key;
@@ -65,12 +64,19 @@ class DoublyLinkedList{
         return k;
         
     }
+    //methis to remove the node
+    public void remove(Node n){
+        n.prev.next = n.next;
+        n.next.prev = n.prev;
+    }
+   
 }
 
 class LRUCache {
-    int capacity;
-    HashMap<Integer, Node> cache;
-    DoublyLinkedList list;
+    int capacity; //define the capacity
+    
+    HashMap<Integer, Node> cache;//hashmap of interger, node 
+    DoublyLinkedList list; //create a linkedlist
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
@@ -80,22 +86,34 @@ class LRUCache {
     }
     
     public int get(int key) {
-    
+        //if cache does not contain key, return -1.
         if(!cache.containsKey(key)){
             return -1;
         }
+        //if contains, the cache will be updated.
         update(key,cache.get(key));
         
+        //return the value required
         return cache.get(key).value;
         
     }
     
+    /*
+    in update, we remove the node,then insert it at front and then put in cache
+    */
     public void update(int key, Node n){
         list.remove(n);
         list.insertHead(n);
         cache.put(key,n);
     }
     
+    //in this put function
+    //create we create new node
+    //remove the node from cache if present
+    //else if cache . size >= k, remove the node from end of list
+    //remove the cache
+    //then add node to front of list
+    //put the key back in cache
     public void put(int key, int value) {
         Node n = new Node(key,value);
         
