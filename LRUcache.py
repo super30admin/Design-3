c5dd72b2370ca25e7bb7697f63949c3c08ea24f4
Shantanu Neema <38290435+shantanuneema@users.noreply.github.com>
2021-02-07 02:37:51 -0800

    Update LRUcache.py

--- LRUcache.py
+++ LRUcache.py
@@ -46,3 +46,71 @@ class LRUCache:
             self.data.pop(0)
            
 # Optimized Solution:
+
+"""
+Still working - not accepted in LeetCode yet
+"""
+
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        
+        # add dummy nodes in begining and end of the linked list
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        
+        # create doubly-linked connection
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def get(self, key: int) -> int:
+        if key in self.cache.keys():
+            node = self.cache[key]
+            self.remove_node(node)
+            self.add_node(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache.keys():
+            print(key, self.cache[key].value)
+            node = self.cache[key]
+            self.remove_node(node)
+            self.add_node(node)
+        else:
+            self.cache[key] = ListNode(key, value)
+            self.add_node(self.cache[key])
+            if len(self.cache) > self.capacity:
+                self.remove_node(self.head.next)
+            
+    def remove_node(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev     
+        
+    def add_node(self, node):
+        # to add node before tail (dummy node)
+        self.tail.prev.next = node
+        node.next = self.tail
+        node.prev = self.tail
+        self.tail.prev = node
+        
+        """ code to add node in front    
+        node.next = self.head.next
+        node.prev = self.head
+        node.next.prev = node
+        self.head.next = node
+        """
+        
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
