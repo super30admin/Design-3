ca7c281eb3fb99be61bef5e8612ef265e311b345
omkar <omkarvaidya13@gmail.com>
2021-03-30 20:06:24 -0400

    Added Comments

--- FlattenNestedIterator.java
+++ FlattenNestedIterator.java
@@ -28,11 +28,16 @@ public class NestedIterator implements Iterator<Integer> {
     }
     
     private void dfs(List<NestedInteger> list){
+        // Edge Case Checking
         if(list == null || list.size() == 0)
             return;
+        
+        // Looping through all the elements of the list
         for(NestedInteger elem : list){
+            // If it is a number --> get its value and add to the queue
             if(elem.isInteger())
                 q.add(elem.getInteger());
+            // If its a list of elements --> go through them (make another dfs call)
             else
                 dfs(elem.getList());
         }
@@ -40,6 +45,7 @@ public class NestedIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
+        // If there is next element present -> poll the queue to get the next element else return -1
         if(hasNext())
             return q.poll();
         else
@@ -48,6 +54,7 @@ public class NestedIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
+        // If the queue is not empty then true else false
         return !q.isEmpty();
     }
 }
