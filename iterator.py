75a7215c838ef6dba2ceafb94b2fd97a6ab07a23
DharmeshNarola <narola.fenil@gmail.com>
2021-08-03 18:21:30 -0500

    design-3

---
+++ iterator.py
@@ -0,0 +1,20 @@
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        ## RC ##
+        ## APPRAOCH : RECURSION / DFS ##
+        
+        self.queue = []
+        self.flattenList(nestedList)
+        
+    def flattenList(self, nestedList):
+        for item in nestedList:
+            if(not item.isInteger()):
+                self.flattenList(item.getList())
+            else:
+                self.queue.append(item.getInteger())
+                
+    def next(self) -> int:
+        return self.queue.pop(0)
+    
+    def hasNext(self) -> bool:
+         return len(self.queue) > 0
