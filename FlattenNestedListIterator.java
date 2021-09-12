265091f93deb7fed274b901d1cf475944d4ed85c
Srinivasa Rao Yedla <ysrinu15@gmail.com>
2021-09-12 14:44:06 -0700

    Design-3 completed

---
+++ FlattenNestedListIterator.java
@@ -0,0 +1,40 @@
+/*
+Time: Next, hasNext is O(1)
+Space: O(DeepestList)
+*/
+public class NestedIterator implements Iterator<Integer> {
+
+    NestedInteger nextEl;
+    Stack<Iterator<NestedInteger>> s; // because we need to do next, hasnext on it
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+
+        s = new Stack<>();
+        s.push(nestedList.iterator());
+
+    }
+
+    @Override
+    public Integer next() {
+        return nextEl.getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        while (!s.isEmpty()) {
+            // case1 - top of stack has been processed
+            if (!s.peek().hasNext())
+                s.pop();
+
+            // case2 - You see an integer, also remember to do peek.next
+            else if ((nextEl = s.peek().next()).isInteger())
+                return true;
+
+            // case3 - You see a list, just push the whole list
+            else
+                s.push(nextEl.getList().iterator());
+        }
+        return false;
+    }
+}
