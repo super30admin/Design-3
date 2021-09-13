3ae2c8bb719263ea4352fd0fb97cc4cdc14d1862
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-13 17:05:49 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -58,3 +58,47 @@ public:
  * NestedIterator i(nestedList);
  * while (i.hasNext()) cout << i.next();
  */
+
+
+
+
+
+// Time Complexity : O(N)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+class NestedIterator {
+public:
+    
+    stack<vector<NestedInteger>::iterator> begins, ends;
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        begins.push(nestedList.begin());
+        ends.push(nestedList.end());
+    }
+    
+    int next() {
+        hasNext();
+        return ((begins.top()++)->getInteger());
+    }
+    
+    bool hasNext() {
+        while(begins.size()){
+            if(begins.top() == ends.top()){
+                begins.pop();
+                ends.pop();
+            }
+            else{
+                auto x = begins.top();
+                if(x->isInteger())
+                    return true;
+                begins.top()++;
+                begins.push(x->getList().begin());
+                ends.push(x->getList().end());
+            }
+        }
+        return false;
+    }
+};
+
+
