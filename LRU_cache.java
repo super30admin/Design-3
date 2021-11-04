134b5e04ac200f230be419d350d53cd0542d00d4
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-11-04 16:58:11 -0400

    Create LRU_cache.java

---
+++ LRU_cache.java
@@ -0,0 +1,74 @@
+class LRUCache {
+    
+    class Node{
+        int key, val;
+        Node prev;
+        Node next;
+        public Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+    
+    private void removeNode(Node node){
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+    }
+    
+    private void addToHead(Node node){
+        node.next = head.next;
+        node.prev = head;
+        head.next = node;
+        node.next.prev = node;
+    }
+    
+    HashMap<Integer, Node> map;
+    Node head; Node tail;
+    int capacity;
+
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        removeNode(node);
+        addToHead(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            node.val = value;
+            removeNode(node);
+            addToHead(node);
+        }else{
+            if(capacity == map.size()){
+                Node tailPrev = tail.prev;
+                removeNode(tailPrev);
+                map.remove(tailPrev.key);
+            }
+            Node newNode = new Node(key, value);
+            addToHead(newNode);
+            map.put(key, newNode);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+
+
+//TC : 0(n)
