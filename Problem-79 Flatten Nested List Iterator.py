56712ec1dd0e5b7dbd189588a278b510195f71d4
Adhish Shrivastava <adhish70@gmail.com>
2021-11-19 10:29:48 -0500

    Initial Commit

---
+++ Problem-79 Flatten Nested List Iterator.py
@@ -0,0 +1,41 @@
+# 341. Flatten Nested List Iterator
+# https://leetcode.com/problems/flatten-nested-list-iterator/
+
+# Time Complexiety: 
+# Space Complexiet: 
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList):
+        pass
+    
+    def next(self) -> int:
+        pass
+    
+    def hasNext(self) -> bool:
+         pass
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
---
+++ Problem-80 LRU Cache.py
@@ -0,0 +1,21 @@
+# 146. LRU Cache
+# https://leetcode.com/problems/lru-cache/
+
+# Time Complexiety: O(1)
+# Space Complexiet: O(capacity)
+        
+class LRUCache:           
+    def __init__(self, capacity: int):
+        pass
+
+    def get(self, key: int) -> int:
+        pass
+
+    def put(self, key: int, value: int) -> None:
+        pass       
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
