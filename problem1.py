0fc22e6f55450513701146276d08845825f360ad
yashgovilkar <yashgovilkar14@gmail.com>
2021-12-07 21:05:12 +0530

    Completed Design-3

---
+++ problem1.py
@@ -0,0 +1,20 @@
+#Time complexity: O(n)
+#Space complexity: O(n)
+#Did this code successfully run on Leetcode : yes
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack= nestedList[::]
+        
+    def next(self) -> int:
+        return self.stack.pop(0).getInteger()
+        
+    def hasNext(self) -> bool:
+        
+        while self.stack:
+            top=self.stack[0]
+            
+            if top.isInteger():
+                return True
+            self.stack = top.getList() + self.stack[1:] 
+            
+        return False
