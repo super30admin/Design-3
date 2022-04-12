6ff7a7a0cece3db1ce5b66d247987794085269f1
architakale <architakale@knights.ucf.edu>
2022-04-11 20:38:58 -0400

    complete design-3

---
+++ Problem1.py
@@ -0,0 +1,55 @@
+# Time Complexity : O(1)
+# Space Complexity : O(dept)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        def gen(nestedList):
+            for x in nestedList:
+                if x.isInteger():
+                    yield x.getInteger()
+                else:
+                    for y in gen(x.getList()):
+                        yield y
+        self.gen = gen(nestedList)
+
+    def next(self):
+        return self.value
+
+    def hasNext(self):
+        try:
+            self.value = next(self.gen)
+            return True
+        except StopIteration:
+            return False
+        
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
