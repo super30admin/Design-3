b849dcd53c7fa437ed5e8ecffa6f4d4567bdbbd5
Sahithi Kamireddy <sahithi.kamireddy@gmail.com>
2022-07-13 17:24:10 -0700

    Completed Design-3

---
+++ FlattenNestedListIterator.java
@@ -0,0 +1,54 @@
+/* Time Complexity : next() - O(1) , hasNext - O(1)
+ * Space Complexity : O(d), where d is depth of largest nested integer
+ * Did this code successfully run on Leetcode : Yes
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+    Stack<Iterator<NestedInteger>> st;
+    NestedInteger nextEl;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        st = new Stack<>();
+        st.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return nextEl.getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!st.isEmpty()){
+            if(!st.peek().hasNext()){
+                st.pop();
+            } else if((nextEl = st.peek().next()).isInteger()){
+                return true;
+            }else{
+                st.push(nextEl.getList().iterator());
+            }
+        }
+        return false;
+    }
+}
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
