59f6b8256f1d81d3b26958576e19187570d64c8c
khushbukareliya07 <khushbu.karelia@gmail.com>
2022-10-24 16:09:01 -0500

    LRU Cache B-F approach notes added

--- LeetCode146_LRUCache.java
+++ LeetCode146_LRUCache.java
@@ -2,7 +2,7 @@
 /*1. create a doubly linkedlist  create HashMap of key as key and node as values. 
 *in doubly linked list the removal and adding to head and tail will cost only O(1)
 * using hashmap will help to lookup for the key in O(1) time, 
-*so tc - O(1), SC - O(n) for using linkedLIst and Map. 
+*so tc - O(1), SC - O(n) for using linkedLIst and Map. n= capcity. 
 */
 
 class LRUCache {
@@ -88,3 +88,19 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+ //approach -2
+ /*this sam approach can be also implemented using a map and a queue
+  * so, map will hold both keys and values
+  *queue will hold keys only. 
+  for every get operation, we will look for the key in Queue, if it exists, we will poll it 
+  and then we will add it back, and then we will return its corresponding value from the map. Well 
+  this operation will definitely O(n) to look for key in queue. 
+  ** for put operation , we will calculate size of the queue for the capacity, and check if key exists, if so we will
+  update the value, but before that, we will poll the key from q, add it back and then update the value. 
+  *let say if key doesn't exist, and we reached the capacity, we poll the last element from the q, and so 
+  corresponding key,val from the map. and after thatwe'll add the new key,val pair to map and key to q. 
+  this approach takes TC - O(n) for both get and put and remove.
+  sc - O(n) for map and queue , n= capacity. 
+   */
