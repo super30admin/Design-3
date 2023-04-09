7de23edd744ea5b4047c9b6172d580559806af5b
Ashmitha <ashdale30@gmail.com>
2023-04-08 19:34:41 -0700

    Done!

---
+++ Problem2.java
@@ -0,0 +1,35 @@
+
+public class Problem2 implements Iterator<Integer> {
+    private Stack<Iterator<NestedInteger>> st;
+    private NestedInteger nextEl;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        st = new Stack<>();
+        st.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return nextEl.getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!st.isEmpty()){
+            if(!st.peek().hasNext())
+                st.pop();
+            else if((nextEl = st.peek().next()).isInteger())
+                return true; 
+            else 
+                st.push(nextEl.getList().iterator());
+
+        }
+        return false;
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
