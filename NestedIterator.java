44bb63e784c797dd5d49acd658dbb8bdd24d207f
Swapnil Katkar <skatkar225@gmail.com>
2023-08-06 04:25:46 +0530

    Update NestedIterator.java

--- NestedIterator.java
+++ NestedIterator.java
@@ -3,44 +3,31 @@ import java.util.List;
 import java.util.Stack;
 
 public class NestedIterator implements Iterator<Integer> {
-
-    Stack<Iterator<NestedInteger>> stack;
-    NestedInteger nextEl;
-
+    Deque<NestedInteger> stack = new ArrayDeque<>();
     public NestedIterator(List<NestedInteger> nestedList) {
-        stack = new Stack<>();
-        stack.push(nestedList.iterator());
+        prepareStack(nestedList);
     }
 
-    // TC: O(d) d - depth of the nested list. It depends on how deeply a list is nested
-    // SC: O(d)
-    // Worst case scenario -> [1,[2,[3,[4,[5,[6]]]]]]
-        // We will need to put each nested list onto the stack which increases both the complexities
-    @Override
-    public boolean hasNext() {
-
-        while(!stack.isEmpty()) {
-            // hasNext & next on stack.top() / stack.peek() object are the native methods since we are storing the iterator in the stack
-                // and not NestedInteger
-            nextEl = stack.peek().next();
-            if(!stack.peek().hasNext()) {
-                // The current iterator for NestedInteger object does not have anything to process
-                // In short, we are done with processing a list (either a sublist of the input list OR the entire input list)
-                stack.pop();
-            }else if(nextEl.isInteger()) {
-                return true;
-            }else {
-                // If the current NestedInteger object is a list then put on top of the stack
-                stack.push(nextEl.getList().iterator());
-            }
-        }
-        return false;
-    }
-
-    // TC: O(1)
-    // SC: O(1)
     @Override
     public Integer next() {
-        return nextEl.getInteger();
+        if (!hasNext()) {
+            return null;
+        }
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+            List<NestedInteger> list = stack.pop().getList();
+            prepareStack(list);
+        }
+        return !stack.isEmpty();
+    }
+    
+    private void prepareStack(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
     }
 }
